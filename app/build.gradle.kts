c86387b031e3206eea68a9c466969896c294b877
514sid <140138716+514sid@users.noreply.github.com>
2025-09-20 17:52:57 +0400

    feat: Update Kotlin and Compose versions, add OkHttp
    This commit updates several dependencies and build configurations:
    
    - Increments `migrationVersion` to `1` in `.idea/gradle.xml`.
    - Updates Kotlin plugin version to `2.2.20`.
    - Updates `androidx.test.ext:junit` to `1.3.0`.
    - Updates `androidx.test.espresso:espresso-core` to `3.7.0`.
    - Updates `androidx.lifecycle:lifecycle-runtime-ktx` to `2.9.4`.
    - Updates `androidx.compose:compose-bom` to `2025.09.00`.
    - Adds `okhttp` (version `5.1.0`) as a dependency in `gradle/libs.versions.toml` and `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +48,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
